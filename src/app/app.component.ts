9ed66d7444badf49c58cc2b5a1d9bbc4fef28321
tushar.dutt <tushar.dutt@newvision-software.com>
2025-02-21 20:35:18 +0530

    Final Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [FormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'basic-form-app';
+  user = { name: '', email: '' };
+  submitted = false;
+
+  onSubmit(form: any) {
+    console.log('Form Data:', form.value);
+    this.submitted = true;
+  }
 }
